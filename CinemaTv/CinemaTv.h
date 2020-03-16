7360c9a9bf71f136e1377fc05c3f4ebe3b0a041e
andrea roveres <andrea.maria.roveres@accenture.com>
2020-03-16 17:22:07 +0100

    Create project, gitignore file and travis file

---
+++ CinemaTv/CinemaTv.h
@@ -0,0 +1,19 @@
+//
+//  CinemaTv.h
+//  CinemaTv
+//
+//  Created by andrea roveres on 16/03/2020.
+//  Copyright © 2020 AndreaRov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CinemaTv.
+FOUNDATION_EXPORT double CinemaTvVersionNumber;
+
+//! Project version string for CinemaTv.
+FOUNDATION_EXPORT const unsigned char CinemaTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CinemaTv/PublicHeader.h>
+
+
